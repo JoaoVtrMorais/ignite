72d2778d1411d03851f4e6531e2c36c9cdf7675c
JoaoVtrMorais <kagunekakuji@outlook.com>
2024-12-06 19:07:06 -0300

    Iniciando com React - Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="João"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro magni veniam eum error dicta suscipit, maiores dolores cupiditate, earum rem similique, quo nisi quae. Doloribus, perspiciatis. Perspiciatis ipsam culpa ratione?"
+      />
+      <Post 
+        author="Diego Fernandes"
+        content="Um novo post muito legal!"
+      />
     </div>
   )
 }
