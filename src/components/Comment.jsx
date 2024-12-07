e09e262a63d511d1871471ffdfde3ce841cc280e
JoaoVtrMorais <kagunekakuji@outlook.com>
2024-12-07 17:10:11 -0300

    Estrutura da aplicação - Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/JoaoVtrMorais.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>João Vitor Morais</strong>
+                            <time title='07 de Dezembro às 08:13h' dateTime='2024-12-07 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+                    <p>
+                        Muito bom Devon, parabéns!! 👏👏
+                    </p>
+                </div>
+                <footer>
+                   <button>
+                    <ThumbsUp />
+                    Aplaudir <span>20</span>
+                   </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
