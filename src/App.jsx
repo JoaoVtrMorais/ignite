1ffe4bdf6dd1bdde35c0de1580fc55f7e767cbbb
JoaoVtrMorais <kagunekakuji@outlook.com>
2024-12-07 15:45:13 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="João"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro magni veniam eum error dicta suscipit, maiores dolores cupiditate, earum rem similique, quo nisi quae. Doloribus, perspiciatis. Perspiciatis ipsam culpa ratione?"
-      />
-      <Post 
-        author="Diego Fernandes"
-        content="Um novo post muito legal!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="João"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro magni veniam eum error dicta suscipit, maiores dolores cupiditate, earum rem similique, quo nisi quae. Doloribus, perspiciatis. Perspiciatis ipsam culpa ratione?"
+          />
+          <Post 
+            author="Diego Fernandes" 
+            content="Um novo post muito legal!" 
+          />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
