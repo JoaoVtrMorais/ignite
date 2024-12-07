7e89a3ada3cf8f80483aa54d12dfb77cbb9b79f4
JoaoVtrMorais <kagunekakuji@outlook.com>
2024-12-07 17:50:58 -0300

    Estrutura da aplicação - Componente: Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/JoaoVtrMorais.png" />
+                    <Avatar src="https://github.com/JoaoVtrMorais.png" />
                     <div className={styles.authorInfo}>
                         <strong>João Vitor Morais</strong>
                         <span>Web Developer</span>    
