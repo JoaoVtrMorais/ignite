8c11c678198df1354c2680c0bb20986281e042ae
JoaoVtrMorais <kagunekakuji@outlook.com>
2024-12-08 16:06:59 -0300

    Os motores do React - Inserindo comentários (Programação declarativa)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content }) {
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
@@ -19,7 +19,7 @@ export function Comment() {
                         </button>
                     </header>
                     <p>
-                        Muito bom Devon, parabéns!! 👏👏
+                        { content }
                     </p>
                 </div>
                 <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,11 +8,11 @@ import styles from './Post.module.css';
 import { useState } from 'react';
 
 export function Post({author, publishedAt, content}) {
-
     const [comments, setComments] = useState([
-        1,
-        2,
+        'Post muito bacana, hein?!'    
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR
@@ -25,12 +25,17 @@ export function Post({author, publishedAt, content}) {
 
     function handleCreateNewComment() {
         event.preventDefault();
-        setComments([...comments, comments.length + 1]);
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value);
     }
 
     return (
         <article className={styles.post}>
-            <header>
+            <header>git
                 <div className={styles.author}>
                     <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
@@ -55,8 +60,11 @@ export function Post({author, publishedAt, content}) {
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea 
+                <textarea
+                    name='comment' 
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
                 <footer>
                     <button type='submit'>Publicar</button>
@@ -65,7 +73,7 @@ export function Post({author, publishedAt, content}) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment content={comment}/>
                 })}
             </div>
         </article>
